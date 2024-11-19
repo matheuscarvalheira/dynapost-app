1f9c6b21fb685ae03700c088b51eced257fecbee
Matheus Carvalheira <matheuscarvalheiraf@gmail.com>
2024-11-18 22:03:59 -0300

    implementing the account form

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,14 @@
+import SignUpForm from "@/components/account-form";
 import Button from "@/components/button";
 import Input from "@/components/input";
 import Post from "@/components/post";
 import { View } from "react-native";
 
 export default function Index() {
+
+  const handleSignUpSubmit = (values: { name: string; email: string; password: string }) => {
+    console.log(values);
+  };
   return (
     <View
       style={{
@@ -12,26 +17,27 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Input label="E-mail" />
+      {/* <Input label="E-mail" />
       <Button onPress={() => {}}>Selecionar</Button>
-      <Post
-        title="Título da Postagem"
-        description="There are many variations of passages of Lorem 
-        Ipsum available, but the majority have suffered alteration 
-        in some form, by injected humour, or random words which 
-        don't look even slightly believable. If you are going to use 
-        a passage of Lorem Ipsum, you need to be sure there isn't 
+       <Post
+         title="Título da Postagem"
+         description="There are many variations of passages of Lorem 
+         Ipsum available, but the majority have suffered alteration 
+         in some form, by injected humour, or random words which 
+         don't look even slightly believable. If you are going to use 
+         a passage of Lorem Ipsum, you need to be sure there isn't 
         anything embarrassing hidden in the middle of text. All the 
-        Lorem Ipsum generators on the Internet tend to repeat 
-        predefined chunks as necessary, making this the first true 
-        generator on the Internet. It uses a dictionary of over 200 
-        Latin words, combined with a handful of model sentence 
+         Lorem Ipsum generators on the Internet tend to repeat 
+         predefined chunks as necessary, making this the first true 
+         generator on the Internet. It uses a dictionary of over 200 
+         Latin words, combined with a handful of model sentence 
         structures, to generate Lorem Ipsum which looks reasonable. 
-        The generated Lorem Ipsum is therefore always free from 
-        repetition, injected humour, or non-characteristic words etc."
-        userType="teacher"
-        userName="Prof. Lorem"
-      />
+         The generated Lorem Ipsum is therefore always free from 
+         repetition, injected humour, or non-characteristic words etc."
+         userType="teacher"
+         userName="Prof. Lorem"
+       /> */}
+      <SignUpForm onSubmit={handleSignUpSubmit} />
     </View>
   );
 }
---
+++ components/account-form/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  padding: 20px;
+  width: 90%;
+`;
+
+export const ErrorText = styled.Text`
+  color: red;
+  margin-bottom: 10px;
+`;
